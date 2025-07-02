7c6d4458c04db1c0add854aa1f0653ebc23c7753
debasisNath <debasis.nath@zeiss.com>
2025-07-02 09:31:27 +0200

    added DQ call to notebook

--- run_etl.sh
+++ run_etl.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DAILY_NOTEBOOK="$SCRIPT_DIR/notebooks/daily_ingest.ipynb"
 MONTHLY_NOTEBOOK="$SCRIPT_DIR/notebooks/monthly_ingest.ipynb"
 BI_NOTEBOOK="$SCRIPT_DIR/notebooks/bi_analytics.ipynb"
-DQ_NOTEBOOK="$SCRIPT_DIR/notebooks/data_quality.ipynb"
+#DQ_NOTEBOOK="$SCRIPT_DIR/notebooks/data_quality.ipynb"
 
 # Run daily ingestion notebook, it takes daily data from prev 24 hr 
 # E.g set it as cron job to run everyday at 8 am : 0 8 * * * /run_etl.sh
@@ -30,6 +30,6 @@ fi
 # Run BI and SQL queries
 papermill "$BI_NOTEBOOK" "$SCRIPT_DIR/output/out_BI_$(date +%F).ipynb" 
            
-# RUN DQ 
-papermill "$DQ_NOTEBOOK" "$SCRIPT_DIR/output/out_DQ_$(date +%F).ipynb" 
+# RUN DQ -> DQ step called from daily_ingest notebook
+# papermill "$DQ_NOTEBOOK" "$SCRIPT_DIR/output/out_DQ_$(date +%F).ipynb" 
            
